35176560e194b9a10f1b939e95fe5d825c7af8b3
Farhan Satria Nugraha <farhansatria.07@gmail.com>
2024-01-04 15:19:39 +0700

    Driver : Add Time.deltaTime()

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,6 +7,7 @@ public class Driver : MonoBehaviour
 
     [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
+
     void Start()
     {
 
@@ -14,8 +15,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
     }
